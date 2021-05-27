f6c0151dd743dfbd07b623076da7543266615eec
John Faucett <john.faucett@studitemps.de>
2021-05-27 11:07:19 +0200

    rename cache to use configuration

--- lib/geocoder/lookups/studitemps.rb
+++ lib/geocoder/lookups/studitemps.rb
@@ -46,19 +46,15 @@ module Geocoder::Lookup
     end
 
     def get_auth0_token
-      return cache.read("auth0_token") if cache&.read("auth0_token")
+      return configuration[:cache_store].read("auth0_token") if configuration[:cache_store]&.read("auth0_token")
 
-      cache&.write("auth0_token", auth0_access_token.token, expires_in: auth0_access_token.expires_in - 10)
+      configuration[:cache_store]&.write("auth0_token", auth0_access_token.token, expires_in: auth0_access_token.expires_in - 10)
       auth0_access_token.token
     end
 
     def auth0_access_token
       @auth0_access_token ||= Auth0.new(**configuration.auth0.slice(:client_id, :client_secret, :site))
                                 .token(audience: configuration.auth0[:audience])
-    end
-
-    def cache
-      configuration[:cache_store]
     end
   end
 end
